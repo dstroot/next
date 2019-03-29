a0f34f308c597087a127a38328c1eaf7a4c68405
Dan Stroot <dan.stroot@gmail.com>
2019-03-29 10:54:49 -0700

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+// This must be the first line in src/index.js
+import 'react-app-polyfill/ie11';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
@@ -8,5 +10,5 @@ ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
