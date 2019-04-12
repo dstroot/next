7a6db59545493d4e136b17076b502c415c4a59d6
Dan Stroot <dan.stroot@gmail.com>
2019-04-12 11:08:47 -0700

    change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import "./scss/main.scss";
+import React from 'react';
+import './scss/main.scss';
 
-const App = () => <div className="lead">Welcome!</div>;
+const App = () => <div className="lead">Welcome Team!</div>;
 
 export default App;
