41fe85da46eeec5b3bd06fe30c184f7a11074cee
vinirocha <vini.rocha96@gmail.com>
2019-04-23 13:01:44 -0700

    card margin

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -9,16 +9,18 @@ import Button from '../Button'
 
 const Card = () => {
   return (
-    <div className="card mx-auto" style={{ width: '18rem' }}>
-      <div className="card-body text-center">
-        <img src={tree} alt="Evergreen" />
-        <h5 className="card-title">Next Deferred Income Annuity</h5>
-        <p className="card-text">
-          Create a future income stream you can count on for the rest of your
-          life by saving over time.
-        </p>
-        <Button buttonText = {"Learn More"} link = {"https:/google.com/"}>
-        </Button>
+    <div className="mt-5">
+      <div className="card mx-auto border-dark mb-3" style={{ width: '18rem' }}>
+        <div className="card-body text-center">
+          <img src={tree} alt="Evergreen"/>
+          <h5 className="card-title">Next Deferred Income Annuity</h5>
+          <p className="card-text">
+            Create a future income stream you can count on for the rest of your
+            life by saving over time.
+          </p>
+          <Button buttonText = {"Learn More"} link = {"https:/google.com/"}>
+          </Button>
+        </div>
       </div>
     </div>
   );
