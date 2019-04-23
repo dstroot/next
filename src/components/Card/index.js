7ec03dbfe3b4f185648ab663e24fda919885c5b3
ericnguyen23 <ericnguyen23@gmail.com>
2019-04-23 14:00:01 -0700

    update card and creted buttoncreated

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,19 +4,25 @@ import './styles.scss';
 // images
 import tree from './media/evergreen.svg';
 
+//components
+import Button from '../Button';
+
 const Card = () => {
   return (
-    <div className="card mx-auto" style={{ width: '18rem' }}>
+    <div
+      className="card mx-auto my-5 card border-dark mb-3 rounded-lg p-3"
+      style={{ width: '24rem' }}
+    >
       <div className="card-body text-center">
         <img src={tree} alt="Evergreen" />
-        <h5 className="card-title">Next Deferred Income Annuity</h5>
-        <p className="card-text">
+        <h5 className="card-title heading-small mb-4">
+          Next Deferred Income Annuity
+        </h5>
+        <p className="card-text circular mb-5">
           Create a future income stream you can count on for the rest of your
           life by saving over time.
         </p>
-        <a href="/" className="card-link">
-          Learn More
-        </a>
+        <Button buttonText="Learn More" link="https://google.com" />
       </div>
     </div>
   );
