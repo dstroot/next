65b75c9ed67fdbb2210525d12327f32a1b2c9f39
ericnguyen23 <ericnguyen23@gmail.com>
2019-04-25 15:53:19 -0700

    created props within Card component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,29 +1,16 @@
 import React from 'react';
 import './styles.scss';
 
-// Router
-import { Router } from '@reach/router';
-
-// images
-import tree from './media/evergreen.svg';
-
 //components
 import Button from '../Button';
 
-import NextDeferredIncomeAnnuity from '../../pages/NextDeferredIncomeAnnuity';
-
-const Card = () => {
+const Card = props => {
   return (
     <div className="card mx-auto my-5 card border-dark mb-3 nxt_rounded-corner p-3 nxt_card-width">
       <div className="card-body text-center">
-        <img src={tree} alt="Evergreen" />
-        <h5 className="card-title nxt_heading-small mb-4">
-          Next Deferred Income Annuity
-        </h5>
-        <p className="card-text circular mb-5">
-          Create a future income stream you can count on for the rest of your
-          life by saving over time.
-        </p>
+        <img src={props.img} alt="Evergreen" className="mb-2" />
+        <h5 className="card-title nxt_heading-small mb-4">{props.title}</h5>
+        <p className="card-text circular mb-5">{props.content}</p>
         <Button path="/nextdeferredincomeannuity" buttonText="Learn More" />
       </div>
     </div>
