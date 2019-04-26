a93cabdbcd274122b3daec4380cc09c43fb8041b
ericnguyen23 <ericnguyen23@gmail.com>
2019-04-26 12:58:52 -0700

    Created Form component, added 600 font weight & finished Design With You In Mind section

--- src/components/SectionAskUs/index.js
+++ src/components/SectionAskUs/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import Form from '../Form';
 
 const SectionAskUs = () => {
   return (
     <div className="nxt_bg-gray py-6">
       <div className="container text-center">
-        <h1>Ask us anything</h1>
-        <h2>Next by Pacific Life</h2>
-        <p>hello@meetnext.com</p>
-        <p>(833) 646-NEXT (6398) </p>
+        <h1 className="nxt_heading-large mb-5">Ask us anything</h1>
+        <div className="row">
+          <div className="col-md-6">
+            <Form />
+          </div>
+          <div className="col-md-6 text-left">
+            <h2>Next by Pacific Life</h2>
+            <a href="mailto: hello@meetnext.com">hello@meetnext.com</a>
+            <p>(833) 646-NEXT (6398) </p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/SectionDesignWithYouInMind/index.js
+++ src/components/SectionDesignWithYouInMind/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// reveal
+import Fade from 'react-reveal/Fade';
+
 // import image
 import womanphone from './media/womanphone.svg';
 
@@ -7,21 +10,42 @@ const SectionDesignWithYouInMind = () => {
   return (
     <div className="py-6">
       <div className="container text-center">
-        <h1 className="nxt_heading-large">Designed with you in mind</h1>
-        <img src={womanphone} alt="Woman with the Phone" />
-
-        <h2>Fearlessly Flexible</h2>
-        <p>
-          Our products adapt to your life’s biggest moments. Change them when
-          your needs do.
-        </p>
-        <h2>Radically Simple</h2>
-        <p>
-          Straight forward products and features. Know what you're getting from
-          the beginning.
-        </p>
-        <h2>Brazenly Transparent</h2>
-        <p>No hidden fees or charges.</p>
+        <Fade bottom>
+          <h1 className="nxt_heading-large mb-5">Designed with you in mind</h1>
+        </Fade>
+        <div className="row">
+          <div className="col-md-6">
+            <Fade bottom>
+              <img src={womanphone} alt="Woman with the Phone" />
+            </Fade>
+          </div>
+          <div className="col-md-6">
+            <Fade bottom>
+              <h2 className="nxt_heading-small mb-2"> Fearlessly Flexible</h2>
+            </Fade>
+            <Fade bottom>
+              <p className="mb-5">
+                Our products adapt to your life’s biggest moments. Change them
+                when your needs do.
+              </p>
+            </Fade>
+            <Fade bottom>
+              <h2 className="nxt_heading-small mb-2">Radically Simple</h2>
+            </Fade>
+            <Fade bottom>
+              <p className="mb-5">
+                Straight forward products and features. Know what you're getting
+                from the beginning.
+              </p>
+            </Fade>
+            <Fade bottom>
+              <h2 className="nxt_heading-small mb-2">Brazenly Transparent</h2>
+            </Fade>
+            <Fade bottom>
+              <p className="mb-5">No hidden fees or charges.</p>
+            </Fade>
+          </div>
+        </div>
       </div>
     </div>
   );
