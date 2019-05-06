6229f0dcfc31240e6165fb02b353c2bf3b26bbd9
ericnguyen23 <ericnguyen23@gmail.com>
2019-05-06 09:05:50 -0700

    Animated svg clouds with css animation

--- src/components/Sections/SectionHero/SectionWhatsYourNext/index.js
+++ src/components/Sections/SectionHero/SectionWhatsYourNext/index.js
@@ -1,34 +1,42 @@
 import React from 'react';
+
 // component
 import SectionHero from '../../SectionHero';
 
 // image
-
 import arrow from './media/arrowdown.svg';
 import moped from './media/moped.svg';
-import bg from './media/mainbg1animationB.svg';
+import bg from './media/city.svg';
+import cloud from './media/cloud.svg';
 
 // styles
 const background = {
-  /* Image  */
   backgroundImage: `url(${bg})`,
 };
 
 const SectionWhatsYourNext = () => {
   return (
-    <SectionHero
-      backgroundImage={background}
-      heading="What’s your next adventure?"
-      content="Insurance products to complement your life goals and passions. We help you go after whatever's next for you."
-      linkElement={
-        <a href="#bestlife" className="text-decoration-none mt-4">
-          <p className="mt-4 mb-2 text-uppercase text-primary">Learn More</p>
-          <img className="d-block-inline" width={45} src={arrow} alt="Arrow" />
-        </a>
-      }
-      heroImage={moped}
-      heroImageAlt="moped"
-    />
+    <div>
+      <SectionHero
+        backgroundImage={background}
+        heading="What’s your next adventure?"
+        content="Insurance products to complement your life goals and passions. We help you go after whatever's next for you."
+        linkElement={
+          <a href="#bestlife" className="text-decoration-none mt-4">
+            <p className="mt-4 mb-2 text-uppercase text-primary">Learn More</p>
+            <img
+              className="d-block-inline"
+              width={45}
+              src={arrow}
+              alt="Arrow"
+            />
+          </a>
+        }
+        heroId="moped"
+        heroImage={moped}
+        heroImageAlt="moped"
+      />
+    </div>
   );
 };
 
