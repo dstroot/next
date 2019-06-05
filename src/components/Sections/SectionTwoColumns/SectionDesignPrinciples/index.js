ac09c73c594b4bd210c16115812fba1bcc1b5a00
ericnguyen23 <ericnguyen23@gmail.com>
2019-06-05 10:46:18 -0700

    mobile pin scroll fixes

--- src/components/Sections/SectionTwoColumns/SectionDesignPrinciples/index.js
+++ src/components/Sections/SectionTwoColumns/SectionDesignPrinciples/index.js
@@ -61,13 +61,13 @@ class SectionDesignPrinciples extends React.Component {
       .from(this.para5, 80, { opacity: 0, y: 100 })
       .from(this.img5, 80, { opacity: 0 }, '-=80');
     new ScrollMagic.Scene({
-      triggerElement: '#scrollStarts',
-      triggerHook: 0.15,
+      triggerElement: '#triggerODP',
+      triggerHook: 0,
       duration: '1000%',
-      offset: 180,
+      offset: -10,
     })
       .setTween(this.tl)
-      .setPin('#scrollStarts')
+      .setPin('#pinODP')
       // .addIndicators() // for debugging
       .addTo(this.controller);
   }
@@ -75,101 +75,109 @@ class SectionDesignPrinciples extends React.Component {
   render() {
     return (
       <div>
-        <div id="scrollStarts">
-          <SectionTwoColumns
-            sectionHeadStyle="text-center mb-5"
-            sectionHead="Our Design Principles"
-            styleLeft="order-2 order-sm-1"
-            styleRight="order-1 order-sm-2"
-            contentLeft={
-              // add spacing to the end of the page since the image is larger then the conteiner
-              <div className="text-center mb-6" id="easel-container">
-                <img id="easel" src={Easel} alt="Design System" width={200} />
-                <img
-                  ref={img => (this.img1 = img)}
-                  className="easel-icon"
-                  src={Lollypop}
-                  alt="Lollypop"
-                  width={65}
-                />
-                <img
-                  ref={img => (this.img2 = img)}
-                  className="easel-icon"
-                  src={Flag}
-                  alt="Flag"
-                  width={90}
-                />
-                <img
-                  ref={img => (this.img3 = img)}
-                  className="easel-icon"
-                  src={PaperPlane}
-                  alt="Paper Plane"
-                />
-                <img
-                  ref={img => (this.img4 = img)}
-                  className="easel-icon"
-                  src={Hi}
-                  alt="Hi"
-                  width={85}
-                />
-                <img
-                  ref={img => (this.img5 = img)}
-                  className="easel-icon"
-                  src={Lock}
-                  alt="Lock"
-                />
-              </div>
-            }
-            noFadeContentRight={
-              <div className="m-4">
-                <div className="mt-2 mq-para" ref={div => (this.para1 = div)}>
-                  <h3 className="nxt_heading-small"> Simplicity </h3>
-                  <p className="pb-1">
-                    We strive to design products that are easy to use and
-                    understand. While it might seem that adding optional
-                    benefits to a product offers flexibility, it can increase
-                    complexity. Keeping the focus on fundamental value results
-                    in a better experience for the customer.
-                  </p>
+        <div id="triggerODP">
+          <div id="pinODP">
+            <SectionTwoColumns
+              sectionHeadStyle="text-center mb-5"
+              sectionHead="Our Design Principles"
+              styleLeft="order-2 order-sm-1"
+              styleRight="order-1 order-sm-2"
+              contentLeft={
+                // add spacing to the end of the page since the image is larger then the conteiner
+                <div className="text-center mb-6" id="easel-container">
+                  <img
+                    id="easel"
+                    className="mq-tall-image"
+                    src={Easel}
+                    alt="Design System"
+                  />
+                  <img
+                    ref={img => (this.img1 = img)}
+                    className="easel-icon"
+                    src={Lollypop}
+                    alt="Lollypop"
+                    width={65}
+                  />
+                  <img
+                    ref={img => (this.img2 = img)}
+                    className="easel-icon"
+                    src={Flag}
+                    alt="Flag"
+                    width={90}
+                  />
+                  <img
+                    ref={img => (this.img3 = img)}
+                    className="easel-icon"
+                    src={PaperPlane}
+                    alt="Paper Plane"
+                  />
+                  <img
+                    ref={img => (this.img4 = img)}
+                    className="easel-icon"
+                    src={Hi}
+                    alt="Hi"
+                    width={85}
+                  />
+                  <img
+                    ref={img => (this.img5 = img)}
+                    className="easel-icon"
+                    src={Lock}
+                    alt="Lock"
+                  />
                 </div>
-                <div className="mt-2 mq-para" ref={div => (this.para2 = div)}>
-                  <h3 className="nxt_heading-small"> Empowerment </h3>
-                  <p className="pb-1">
-                    Reinforcing and celebrating positive actions toward
-                    customer’s financial health helps them realize success. We
-                    give customers confidence in going after their passions and
-                    goals.
-                  </p>
+              }
+              noFadeContentRight={
+                <div className="m-4" id="mq-para-container">
+                  <div className="mt-2 mq-para" ref={div => (this.para1 = div)}>
+                    <h3 className="nxt_heading-small"> Simplicity </h3>
+                    <p className="pb-1">
+                      We strive to design products that are easy to use and
+                      understand. While it might seem that adding optional
+                      benefits to a product offers flexibility, it can increase
+                      complexity. Keeping the focus on fundamental value results
+                      in a better experience for the customer.
+                    </p>
+                  </div>
+                  <div className="mt-2 mq-para" ref={div => (this.para2 = div)}>
+                    <h3 className="nxt_heading-small"> Empowerment </h3>
+                    <p className="pb-1">
+                      Reinforcing and celebrating positive actions toward
+                      customer’s financial health helps them realize success. We
+                      give customers confidence in going after their passions
+                      and goals.
+                    </p>
+                  </div>
+                  <div className="mt-2 mq-para" ref={div => (this.para3 = div)}>
+                    <h3 className="nxt_heading-small"> Flexibility </h3>
+                    <p className="pb-1">
+                      We recognize that customers’ needs change over time.
+                      Adopting a fluid mindset helps us design products that
+                      adapt to the customer over time.
+                    </p>
+                  </div>
+                  <div className="mt-2 mq-para" ref={div => (this.para4 = div)}>
+                    <h3 className="nxt_heading-small"> Engagement </h3>
+                    <p className="pb-1">
+                      Customers expect that we will be around for their entire
+                      lives, but traditionally most contact is at time of
+                      purchase and claims. We interact with customers in a
+                      meaningful and helpful way throughout the lifelong
+                      partnership.
+                    </p>
+                  </div>
+                  <div className="mt-2 mq-para" ref={div => (this.para5 = div)}>
+                    <h3 className="nxt_heading-small"> Transparency </h3>
+                    <p className="pb-">
+                      We believe in an open and honest dialogue. Our industry
+                      may be complex, but we choose to always work in the best
+                      interest of the customer. That means we have to be clear
+                      about how our products work and the value they provide.
+                    </p>
+                  </div>
                 </div>
-                <div className="mt-2 mq-para" ref={div => (this.para3 = div)}>
-                  <h3 className="nxt_heading-small"> Flexibility </h3>
-                  <p className="pb-1">
-                    We recognize that customers’ needs change over time.
-                    Adopting a fluid mindset helps us design products that adapt
-                    to the customer over time.
-                  </p>
-                </div>
-                <div className="mt-2 mq-para" ref={div => (this.para4 = div)}>
-                  <h3 className="nxt_heading-small"> Engagement </h3>
-                  <p className="pb-1">
-                    Customers expect that we will be around for their entire
-                    lives, but traditionally most contact is at time of purchase
-                    and claims. We interact with customers in a meaningful and
-                    helpful way throughout the lifelong partnership.
-                  </p>
-                </div>
-                <div className="mt-2 mq-para" ref={div => (this.para5 = div)}>
-                  <h3 className="nxt_heading-small"> Transparency </h3>
-                  <p className="pb-">
-                    We believe in an open and honest dialogue. Our industry may
-                    be complex, but we choose to always work in the best
-                    interest of the customer. That means we have to be clear
-                    about how our products work and the value they provide.
-                  </p>
-                </div>
-              </div>
-            }
-          />
+              }
+            />
+          </div>
         </div>
       </div>
     );
